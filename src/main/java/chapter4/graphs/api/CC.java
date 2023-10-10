8dac116e5a142c602db44a27e52a7196b32ed268
Fish <296789191@qq.com>
2023-10-10 17:29:41 +0800

    feat(图): DFS实现CC API

---
+++ src/main/java/chapter4/graphs/api/CC.java
@@ -0,0 +1,28 @@
+package chapter4.graphs.api;
+
+/**
+ * 连通分量 API
+ */
+public abstract class CC {
+
+    public Graph graph;
+
+    public CC(Graph G){
+        this.graph = G;
+    }
+
+    /**
+     * v与w是否连通
+     */
+    public abstract boolean connected(int v, int w);
+
+    /**
+     * 连通分量数
+     */
+    public abstract int count();
+
+    /**
+     * 连通分量ID
+     */
+    public abstract int id(int v);
+}
