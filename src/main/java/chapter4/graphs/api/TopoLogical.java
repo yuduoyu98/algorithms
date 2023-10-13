83a29de05aa17301456b4f5f4db5d6d263c91240
YuShaoyu <296789191@qq.com>
2023-10-13 21:27:26 +0800

    feat(图): 有向图DFS遍历顶点输出顺序

---
+++ src/main/java/chapter4/graphs/api/TopoLogical.java
@@ -0,0 +1,18 @@
+package chapter4.graphs.api;
+
+/**
+ * 拓扑排序 API
+ * 有向无环图 DAG 才会有拓扑排序
+ */
+public abstract class TopoLogical {
+
+    protected DirectedGraph graph;
+
+    public TopoLogical(DirectedGraph G){
+        graph = G;
+    }
+
+    public abstract boolean isDAG();
+
+    public abstract Iterable<Integer> order();
+}
