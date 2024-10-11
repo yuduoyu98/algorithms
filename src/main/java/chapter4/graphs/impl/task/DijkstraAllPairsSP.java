490c723cd5e4b0730ffd307729d7769f2c9f8844
Fish <296789191@qq.com>
2024-10-11 19:03:55 +0800

    feat(Dijkastra Algorithm): Dijkastra algorithm to solve all pairs shortest path problem

---
+++ src/main/java/chapter4/graphs/impl/task/DijkstraAllPairsSP.java
@@ -0,0 +1,24 @@
+package chapter4.graphs.impl.task;
+
+import chapter4.graphs.impl.ds.EdgeWeightedDiGraph;
+import chapter4.graphs.impl.ds.WeightedDiEdge;
+
+
+/**
+ * shortest paths solution for all possible pairs of source and terminal
+ * - time cost: O(EVlogV)
+ */
+public class DijkstraAllPairsSP {
+    private DijkstraSPT[] source;
+
+    DijkstraAllPairsSP(EdgeWeightedDiGraph G) {
+        source = new DijkstraSPT[G.V()];
+        for (int v = 0; v < G.V(); v++)
+            source[v] = new DijkstraSPT(G, v);
+    }
+
+    public Iterable<WeightedDiEdge> path(int s, int t) {return source[s].pathTo(t);}
+
+    public double dist(int s, int t) {return source[s].distTo(t);}
+
+}
