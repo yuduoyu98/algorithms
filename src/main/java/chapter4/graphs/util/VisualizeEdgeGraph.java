5e2c20a1754a3bbe8de23f96ca149818bac8713d
Fish <296789191@qq.com>
2024-10-11 21:02:58 +0800

    feat(Graph Visualization): edge-weighted directed graph visualization

--- src/main/java/chapter4/graphs/util/VisualizeEdgeGraph.java
+++ src/main/java/chapter4/graphs/util/VisualizeEdgeGraph.java
@@ -1,6 +1,10 @@
 package chapter4.graphs.util;
 
+import chapter1.fundamentals.api.Queue;
+import chapter1.fundamentals.impl.SimpleQueue;
+import chapter4.graphs.impl.ds.EdgeWeightedDiGraph;
 import chapter4.graphs.impl.ds.EdgeWeightedGraph;
+import chapter4.graphs.impl.ds.WeightedDiEdge;
 import chapter4.graphs.impl.ds.WeightedEdge;
 import edu.princeton.cs.algs4.In;
 
@@ -18,22 +22,59 @@ public class VisualizeEdgeGraph {
     private static final String PIC_TYPE = "png";
     private static final String DIRECTED_EDGE_MARK = "->";
     private static final String UNDIRECTED_EDGE_MARK = "--";
-    private EdgeWeightedGraph G;
+    private Queue<Edge> edges;
     private String outDir;
     private String fileName;
+    private boolean isDirected;
 
     private static final String FALSE = "false";
     private static final String TRUE = "true";
 
+    private static class Edge {
+        private int x;
+        private int y;
+        private double weight;
+
+        public Edge(WeightedEdge edge) {
+            this.x = edge.start;
+            this.y = edge.end;
+            this.weight = edge.weight();
+        }
+
+        private Edge(WeightedDiEdge edge) {
+            this.x = edge.from();
+            this.y = edge.to();
+            this.weight = edge.weight();
+        }
+    }
+
+    @SuppressWarnings("unused")
     public VisualizeEdgeGraph(EdgeWeightedGraph graph, String fileName) {
         this(graph, DEFAULT_OUTPUT_DIR, fileName);
     }
 
     public VisualizeEdgeGraph(EdgeWeightedGraph graph, String output, String fileName) {
-        this.G = graph;
         this.outDir = output;
         this.fileName = fileName;
+        this.edges = new SimpleQueue<>();
+        this.isDirected = false;
+        for (WeightedEdge edge : graph.edges())
+            edges.enqueue(new Edge(edge));
     }
+
+    public VisualizeEdgeGraph(EdgeWeightedDiGraph graph, String fileName) {
+        this(graph, DEFAULT_OUTPUT_DIR, fileName);
+    }
+
+    public VisualizeEdgeGraph(EdgeWeightedDiGraph graph, String output, String fileName) {
+        this.outDir = output;
+        this.fileName = fileName;
+        this.edges = new SimpleQueue<>();
+        this.isDirected = true;
+        for (WeightedDiEdge edge : graph.edges())
+            edges.enqueue(new Edge(edge));
+    }
+
 
     /**
      * dot file generation
@@ -42,12 +83,14 @@ public class VisualizeEdgeGraph {
     private void dotGen() {
         // init dot file
         StringBuilder sb = new StringBuilder();
-        String header = "strict graph" + " g {\n";
+        String header = (isDirected ? "digraph" : "strict graph") + " g {\n";
+
         sb.append(header);
         // adjacent list
-        for (int v = 0; v < G.V(); v++)
-            for (WeightedEdge edge : G.adj(v))
-                addEdge(sb, edge);
+        for (Edge edge : edges) {
+            addEdge(sb, edge);
+        }
+
         addSetting(sb, "overlap", FALSE);
         addSetting(sb, "splines", TRUE);
         addSetting(sb, "sep", ".1");
@@ -55,11 +98,10 @@ public class VisualizeEdgeGraph {
         dotWriter(sb.toString());
     }
 
-    private void addEdge(StringBuilder sb, WeightedEdge edge) {
-        int v = edge.either();
-        int w = edge.other(v);
-        sb.append("\t").append(v).append(UNDIRECTED_EDGE_MARK).append(w)
-                .append("[label=\"").append(edge.weight()).append("\"]").append(";\n");
+    private void addEdge(StringBuilder sb, Edge edge) {
+        String edgeMark = isDirected ? DIRECTED_EDGE_MARK : UNDIRECTED_EDGE_MARK;
+        sb.append("\t").append(edge.x).append(edgeMark).append(edge.y)
+                .append("[label=\"").append(edge.weight).append("\"]").append(";\n");
     }
 
     private void addSetting(StringBuilder sb, String key, String val) {
@@ -113,10 +155,12 @@ public class VisualizeEdgeGraph {
 
     // test example
     public static void main(String[] args) {
-        String filename = "tinyEWG";
+//        String filename = "tinyEWG";
+        String filename = "tinyEWDAG";
         File file = new File("C:\\Users\\Fish\\Desktop\\algorithms\\src\\test\\java\\chapter4\\graphs\\data\\" + filename + ".txt");
         In in = new In(file);
-        EdgeWeightedGraph G = new EdgeWeightedGraph(in);
+//        EdgeWeightedGraph G = new EdgeWeightedGraph(in);
+        EdgeWeightedDiGraph G = new EdgeWeightedDiGraph(in);
         VisualizeEdgeGraph test = new VisualizeEdgeGraph(G, filename);
         test.picGen();
     }
