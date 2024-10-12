95fe1d7fe99ff79e9535f0a0179a9b8f35514b3d
YuShaoyu <296789191@qq.com>
2024-10-12 23:34:39 +0800

    perf(Topological): Topological revision

--- src/test/java/chapter4/graphs/TestTopological.java
+++ src/test/java/chapter4/graphs/TestTopological.java
@@ -7,6 +7,8 @@ import chapter4.graphs.impl.ds.AdjListDGraph;
 import chapter4.graphs.impl.task.DFSTopological;
 import chapter4.graphs.impl.task.KahnTopological;
 import chapter4.graphs.impl.ds.SymbolGraphForJobs;
+import common.DataSize;
+import common.TestData;
 import edu.princeton.cs.algs4.StdOut;
 import org.junit.Before;
 import org.junit.Test;
@@ -22,6 +24,7 @@ import java.lang.reflect.InvocationTargetException;
 public class TestTopological {
 
     private SymbolGraph<AdjListDGraph> sg;
+    private DirectedGraph dg;
 
     private Class<? extends Topological> implClazz;
 
@@ -32,6 +35,13 @@ public class TestTopological {
     @Before
     public void generateGraph() {
         sg = new SymbolGraphForJobs("src/test/java/chapter4/graphs/data/jobs.txt");
+        TestData testData = new TestData(
+                "https://algs4.cs.princeton.edu/42digraph/",
+                "src/test/java/chapter4/graphs/data/",
+                "tinyDAG.txt",
+                null,
+                null);
+        dg = new AdjListDGraph(testData.getIn(DataSize.TINY, true));
     }
 
     @Parameterized.Parameters
@@ -43,9 +53,34 @@ public class TestTopological {
     }
 
     @Test
-    public void topoTest(){
-        System.out.println("---------------------------------");
-        StdOut.println("Test Class： " + implClazz);
+    public void dgTest(){
+        System.out.println("----------------directed graph test-----------------");
+        StdOut.println("Test Class： " + implClazz.getSimpleName());
+        Topological topo;
+        try {
+            topo = implClazz.getConstructor(DirectedGraph.class).newInstance(dg);
+        } catch (InstantiationException | IllegalAccessException | InvocationTargetException |
+                NoSuchMethodException e) {
+            throw new RuntimeException(e);
+        }
+        if (!topo.isDAG()) {
+            StdOut.print("not ");
+        }
+        StdOut.println("DAG");
+        if (topo.isDAG()) {
+            StdOut.println("Topological sort: ");
+            for (int v : topo.order()) {
+                StdOut.print(v + " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+
+    @Test
+    public void sgTest(){
+        System.out.println("----------------symbol graph test-----------------");
+        StdOut.println("Test Class： " + implClazz.getSimpleName());
         Topological topo;
         try {
             topo = implClazz.getConstructor(DirectedGraph.class).newInstance(sg.G());
